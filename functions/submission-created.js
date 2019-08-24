3ca802b7c9f79fa48a4c6f5cadc7fbf72974f8af
hikangg <kangg1987@hotmail.com>
2019-08-25 00:31:35 +0800

    toml file changed

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,5 +1,8 @@
+import querystring from "querystring";
+
 exports.handler = function(event, context, callback) {
-    console.log(event, '~~~~~~~')
+    const params = querystring.parse(event.body);
+    console.log(params, '============')
     callback(null, {
         statusCode: 200,
         body: 'No worries, all is working fine!'
