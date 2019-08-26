32caee5181c441511d99b7cd82a90b500612cded
hikangg <kangg1987@hotmail.com>
2019-08-26 22:35:16 +0800

    lambda func added

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,9 +3,6 @@ import querystring from "querystring";
 exports.handler = function(event, context, callback) {
     const params = querystring.parse(event.body);
     const queries = querystring.parse(event.queryStringParameters);
-    console.log(params, '~~~~~~~~~~')
-    console.log(queries, '<<<<<<<<<<')
-    console.log(context, '>>>>>>>>')
     callback(null, {
         statusCode: 200,
         body: 'No worries, all is working fine!'
